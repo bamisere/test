168300de8c3874b4bdaf9a443606fdf11d44b6c7
bamisere <42433199+bamisere@users.noreply.github.com>
2023-01-04 06:31:25 +0000

    Update Database.php

--- class/Database.php
+++ class/Database.php
@@ -3,7 +3,9 @@ class Database {
     public function dbConnect() {        
         static $DBH = null;      
         if (is_null($DBH)) {              
-			$connection = new mysqli(HOST, USER, PASSWORD, DATABASE);			
+		$connection = mysqli_init();
+            		mysqli_ssl_set($connection,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+             		mysqli_real_connect($connection, "mav-server.mysql.database.azure.com", "mysqladmin@mav-server", "Bambam12", "helpdesk_system", 3306);			
 			if($connection->connect_error){
 				die("Error failed to connect to MySQL: " . $connection->connect_error);
 			} else {
